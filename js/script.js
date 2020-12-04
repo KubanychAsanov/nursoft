a78db5e32b71072ac92d5b910cdad8c83b8fd18e
Kuba <kuba.asanov4@gmail.com>
2020-12-04 18:16:56 +0600

    added scroll

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ function openmenu(){
     element1.classList.toggle('active')
 }
 
+let wrapper = document.querySelector('.wrapper');
+
 let pageSlider = new Swiper('.page',{
     
     wrapperClass: "page__wrapper",
@@ -50,6 +52,69 @@ let pageSlider = new Swiper('.page',{
         el: '.page__scroll',
         dragClass: "page__drag-scroll",
         draggable: true
+    },
+    init: false,
+    on: {
+        init: function(){
+            menuSlider();
+            setScrollType()
+            wrapper.classList.add('_loaded');
+        },
+        slideChange: function(){
+            menuSliderRemove();
+            menuLinks[pageSlider.realIndex].classList.add('_active');
+        } ,
+        resize: function(){
+            setScrollType();
+        }
     }
 
 })
+
+let menuLinks = document.querySelectorAll('.menu__link');
+
+function menuSlider(){
+    if(menuLinks.length > 0){
+        menuLinks[pageSlider.realIndex].classList.add('_active');  
+        for(let index = 0; index<menuLinks.length; index++){
+            const menuLink = menuLinks[index];
+            menuLink.addEventListener("click", function(e){
+                menuSliderRemove(); 
+                pageSlider.slideTo(index, 800);
+                menuLink.classList.add('_active');
+                e.preventDefault(); 
+            })
+        }
+    }
+}
+
+function menuSliderRemove(){
+    let menuLinkActive = document.querySelector('.menu__link._active');
+    if(menuLinkActive){
+        menuLinkActive.classList.remove('_active');
+    }
+}
+function setScrollType(){
+    if (wrapper.classList.contains('_free')) {
+        wrapper.classList.remove('_free');
+        pageSlider.params.freeMode = false;
+    }
+    for (let index = 0; index < pageSlider.slides.length; index++) {
+        console.log(pageSlider.slides.length);
+        console.log(pageSlider.slides[index]);
+        const pageSlide = pageSlider.slides[index];
+        const pageSlideContent = pageSlide.querySelector('.screen__content');
+        console.log(pageSlideContent)
+        if (pageSlideContent) {
+            const pageSlideContentHeight = pageSlideContent.offsetHeight;
+            console.log("page height " + pageSlideContentHeight);
+            console.log("window height " + window.innerHeight);
+            if (pageSlideContentHeight > window.innerHeight) {
+                wrapper.classList.add('_free');
+                pageSlider.params.freeMode = true;
+                break;
+            }
+        } 
+    }
+}
+pageSlider.init();
